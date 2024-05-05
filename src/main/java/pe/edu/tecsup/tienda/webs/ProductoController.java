101dbcfa187e01374894797aebfe2bfe17942916
jgomezz <jgomezz@gmail.com>
2024-05-05 12:57:53 -0500

    create producto

--- src/main/java/pe/edu/tecsup/tienda/webs/ProductoController.java
+++ src/main/java/pe/edu/tecsup/tienda/webs/ProductoController.java
@@ -3,13 +3,16 @@ package pe.edu.tecsup.tienda.webs;
 import lombok.extern.java.Log;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import pe.edu.tecsup.tienda.entities.Categoria;
 import pe.edu.tecsup.tienda.entities.Producto;
 import pe.edu.tecsup.tienda.services.CategoriaService;
 import pe.edu.tecsup.tienda.services.ProductoService;
 
+import java.util.Date;
 import java.util.List;
 
 @Log
@@ -49,4 +52,31 @@ public class ProductoController {
 
         return "productos/create";
     }
+
+    @PostMapping("/store")
+    public String store(@ModelAttribute("producto") Producto producto, Errors errors,
+                        @RequestParam("file") MultipartFile file,
+                        RedirectAttributes redirectAttrs) throws Exception{
+        log.info("call store(producto: " + producto + ")");
+
+        /*if(file != null && !file.isEmpty()) {
+            String filename = System.currentTimeMillis() + file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+            producto.setImagen_nombre(filename);
+            if(Files.notExists(Paths.get(STORAGEPATH))){
+                Files.createDirectories(Paths.get(STORAGEPATH));
+            }
+            Files.copy(file.getInputStream(), Paths.get(STORAGEPATH).resolve(filename), StandardCopyOption.REPLACE_EXISTING);
+        }*/
+
+        producto.setCreado(new Date());
+        producto.setEstado(1);
+
+        productoService.save(producto);
+
+        redirectAttrs.addFlashAttribute("message", "Registro guardado correctamente");
+
+        return "redirect:/productos";
+    }
+
+
 }
