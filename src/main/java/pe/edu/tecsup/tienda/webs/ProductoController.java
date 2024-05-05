f61f760a40ddf22e9c407da12a4e1ac5baea9008
jgomezz <jgomezz@gmail.com>
2024-05-05 11:25:56 -0500

    update controller class

---
+++ src/main/java/pe/edu/tecsup/tienda/webs/ProductoController.java
@@ -0,0 +1,28 @@
+package pe.edu.tecsup.tienda.webs;
+
+import lombok.extern.java.Log;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import pe.edu.tecsup.tienda.services.CategoriaService;
+import pe.edu.tecsup.tienda.services.ProductoService;
+
+@Log
+@Controller
+public class ProductoController {
+
+    CategoriaService categoriaService;
+
+    ProductoService productoService;
+
+    public ProductoController(CategoriaService categoriaService, ProductoService productoService) {
+        this.categoriaService = categoriaService;
+        this.productoService = productoService;
+    }
+
+    @GetMapping("/productos")
+    public String index(Model model) {
+
+        return "productos/index";
+    }
+}
